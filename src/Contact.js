b12404f853467c3cda154b39ec0b822a22590e0a
Vikas Bhalla <vikasbhalla36@gmail.com>
2022-10-25 13:22:17 +0530

    #9: Contact Form Send Email & Google Map

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from "react";
 import styled from "styled-components";
 
 const Contact = () => {
@@ -34,7 +34,61 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact Us</h2>
+
+      {/* map iframe */}
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227749.0004237037!2d75.65012591706504!3d26.885141337582652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396c4adf4c57e281%3A0xce1c63a0cf22e09!2sJaipur%2C%20Rajasthan!5e0!3m2!1sen!2sin!4v1666682159284!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Google Map"
+      ></iframe>
+
+      {/* contact form */}
+
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/mrgdyeye" method="post" className="contact-inputs">
+            <input
+              type="text"
+              name="username"
+              id="name"
+              placeholder="username"
+              required
+              autoComplete="off"
+              className="form-input"
+            />
+            <input
+              type="email"
+              name="Email"
+              id="email"
+              placeholder="user@host.com"
+              required
+              autoComplete="off"
+              className="form-input"
+            />
+            <textarea
+              name="Message"
+              id="message"
+              placeholder="Enter your message"
+              cols="30"
+              rows="10"
+              autoComplete="off"
+              className="form-input"
+            ></textarea>
+
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
