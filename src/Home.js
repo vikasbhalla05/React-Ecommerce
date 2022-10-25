3d5cbe419e1c68d0e7e5f87f609130b2db56e05a
Vikas Bhalla <vikasbhalla36@gmail.com>
2022-10-25 10:02:06 +0530

    #8: Complete Services & Trusted Section

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 // import styled from 'styled-components'
 import HeroSection from './components/HeroSection';
+import Services from './components/Services';
+import Trusted from './components/Trusted'
 
 const Home = () => {
   return (
     <>
       <HeroSection name="Ecommerce Home" />
+      <Services />
+      <Trusted />
     </>
   );
 }
