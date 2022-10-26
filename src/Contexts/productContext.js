24240caa4e57406788420abc2f101da6517b9db4
Vikas Bhalla <vikasbhalla36@gmail.com>
2022-10-26 07:21:13 +0530

    #13: Fetch API data using Axios

--- src/Contexts/productContext.js
+++ src/Contexts/productContext.js
@@ -1,11 +1,24 @@
 // create a context
 
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 const AppContext = createContext();
 
 // Provider
 
+const API = "https://api.pujakaitem.com/api/products"
+
 const AppProvider = ({children}) => {
+
+    const getProducts = async (url) => {
+        let fetchData = await axios.get(url);
+        console.log(fetchData.data)
+    }
+
+    useEffect(() => {
+      getProducts(API);
+    }, [])
+    
     return <AppContext.Provider value={{ myName: "Here" }}>
         {children}
     </AppContext.Provider>;
